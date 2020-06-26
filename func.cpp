ef31da5cdb9d6caacd522f838ac548b7496fb94e
Adi-Levy <adilevy15@gmail.com>
2020-06-26 12:43:19 +0300

    started implementing func.cpp

--- func.cpp
+++ func.cpp
@@ -1,77 +1,102 @@
+#include <math.h>
+#include <vector>
 #include "func.h"
 
 
+
+func::func() : maxVal_(), minVal_() {
+    map<int, int> fmap_;
+}
+
 void func::plot(ostream& os) const {
  
-  vector<int> sortImage;
+    std::vector<int> sortImage;
     
-  sortImage.clear();
-  for ( auto it : fmap_){
-    //complete code here: insert the image of the function into sortImage 
-  }
-  //complete code here: sort sortImage
-  //complete code here: flip sortImage (reverse)
+    sortImage.clear();
+    for ( auto it : fmap_){
+        //complete code here: insert the image of the function into sortImage
+        sortImage.push_back(it.second);
+    }
+    //complete code here: sort sortImage
+    //complete code here: flip sortImage (reverse)
   
-  for ( auto it_im = sortImage.begin();
+    for ( auto it_im = sortImage.begin();
        it_im != sortImage.end(); ++it_im) {
-    if(it_im!=sortImage.begin() && *it_im==*(it_im-1)){ //remove repeated
-      it_im = sortImage.erase(it_im)-1;
+        if(it_im!=sortImage.begin() && *it_im==*(it_im-1)){ //remove repeated
+            it_im = sortImage.erase(it_im)-1;
       
+        }
     }
-  }
  
-  for (auto it_im = sortImage.begin();
+    for (auto it_im = sortImage.begin();
        it_im != sortImage.end(); ++it_im) {
-    int x_anchor=minVal_;
-    if(*it_im<-9) 
-      os <<*it_im;
-    else
-      if((*it_im<0) || (*it_im>9)) 
-	os<<" "<<*it_im;
-      else 
-	os<<"  "<<*it_im;
-    
-    for (auto it_dom /*complete code here: loop over map*/) {
-      if(it_dom.second == *it_im){
-	int x=it_dom.first;
-	int spaces= x-x_anchor;
-	int i=0;
-	while(i<spaces){
-	  os<<"   ";
-	  i++;
-	}
-	os<<"*  ";
-	x_anchor=x+1;
-      }
+        // print y axis values
+        int x_anchor=minVal_;
+        if(*it_im<-9) 
+            os <<*it_im;
+        else
+            if((*it_im<0) || (*it_im>9)) 
+	            os<<" "<<*it_im;
+            else 
+	            os<<"  "<<*it_im;
+        
+        // print function point
+        for (auto it_dom : fmap_/*complete code here: loop over map*/) {
+            if(it_dom.second == *it_im){
+	            int x=it_dom.first;
+	            int spaces= x-x_anchor;
+	            int i=0;
+	            while(i<spaces){
+	                os<<"   ";
+	                i++;
+	            }
+	            os<<"*  ";
+	            x_anchor=x+1;
+            }
 
-    }
+        }
 
-    os<<endl;
-    if((it_im+1)!=sortImage.end()){ //print empty lines
-      int lines=*it_im-*(it_im+1)-1;
-      int i=1;
+        os<<std::endl;
+        if((it_im+1)!=sortImage.end()){ //print empty lines
+            int lines=*it_im-*(it_im+1)-1;
+            int i=1;
       
-      while(i<lines+1){
-	if(*it_im-i<-9) 
-	  os <<*it_im-i;
-	else 
-	  if( (*it_im-i<0) || (*it_im-i>9) )
-	    os<<" "<<*it_im-i;
-	  else 
-	    os<<"  "<<*it_im-i;
-	cout<<endl;
-	i++;
+            while(i<lines+1){
+	            if(*it_im-i<-9) 
+	                os <<*it_im-i;
+	            else 
+	                if( (*it_im-i<0) || (*it_im-i>9) )
+	                    os<<" "<<*it_im-i;
+	                else 
+	                    os<<"  "<<*it_im-i;
+	            os<<std::endl;
+	            i++;
 
-      }
+            }
+        }
+
+    }//for sortImage
+
+    //print x axis
+    os<< " ";
+    for (auto i=minVal_; i<maxVal_+1;i++){
+        if(i<0) os<<" "<<i;
+        else os<<"  "<<i;
     }
+    os<<std::endl;
+}
 
-  }//for sortImage
+func& func::operator<<(const int& x) {
+    std::pair<int, int> p;
+    p.first = x;
+    p.second = f(x);
+    fmap_.insert(p);
+    if (fmap_.at(x) == p.second) {
+        if(minVal_
+    }
+    return *this;
+}
 
-  //print x axis
-  cout<< " ";
-  for (auto i=minVal_; i<maxVal_+1;i++){
-    if(i<0) os<<" "<<i;
-    else os<<"  "<<i;
-  }
-  os<<endl;
+ostream& operator<<(ostream& os, const func& f) {
+    
 }
--- func.h
+++ func.h
@@ -10,16 +10,15 @@ using std::ostream;
 
 
 class func {
- 
-
-
- protected:
-   
-  
-  int maxVal_; //maximum of all inputs
-  int minVal_; //minimum of all inputs
-  map<int,int> fmap_; //holds inputs and corresponding outputs
-  void plot(ostream& os) const ; //plot fmap_
+protected:
+    int maxVal_; //maximum of all inputs
+    int minVal_; //minimum of all inputs
+    map<int,int> fmap_; //holds inputs and corresponding outputs
+    void plot(ostream& os) const ; //plot fmap_
+    virtual int f(const int& x) = 0;
+public:
+    friend ostream& operator<<(ostream&, const func&);
+    func& operator<<(const int& x);
 };
 
 
